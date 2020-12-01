9c8c08f697f3f1924ac87181b2c6f4a49cedd972
Airton1160 <liriungo@outlook.com.br>
2020-12-01 20:24:17 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from 'next'
+import Link from 'next/link'
+
+const index = () => {
+    return (
+        <div>
+            <div>
+                <Link href="/NV pagina">
+                    <a>NOVA PAGINA</a>
+                </Link>
+            </div>
+        </div>
+    )
+}
+export default index
